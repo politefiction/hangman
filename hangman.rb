db34ac031022b3287d4ca702b761618b151e13d6
Chelsi Jones <cajones.bz@gmail.com>
2017-12-14 09:59:01 -0600

    Implements basic game mechanics in Sinatra

--- hangman.rb
+++ hangman.rb
@@ -1,9 +1,72 @@
+require 'sinatra'
+require 'sinatra/reloader'
 require 'json'
+require 'erb'
+
+#set :save_game, Hangman.from_json
+#set :new_game, Hangman.new
+set :game, nil
+enable :sessions
+
+
+# Okay, okay: so, parameters in the html are defined by what I put into the forms. That's why
+# params["game"] was nil. Got it.
+
+get '/' do
+	@answer1 = params["answer1"]
+	@message2 = session.delete(:message)
+	settings.game ||= start_game(@answer1) if @answer1
+	#@message2
+	erb :index , :locals => { :game => settings.game }
+end
+
+def start_game(answer)
+	if answer[0].downcase == "y"
+		@message1 = "Previous save loaded."
+		Hangman.from_json
+	elsif answer[0].downcase == "n"
+		@message1 = "New game loaded."
+		Hangman.new
+	else
+		@message1 = "Sorry, unable to recognize your response. Please try again."
+		nil
+		#redirect "/"
+	end
+end
+
+post '/' do 
+	@guess = params["guess"]
+	@game = settings.game
+	@game.enter_guess(@guess)
+	session[:message] = assign_message
+	@game.already_guessed << @guess unless @guess == "save"
+	redirect "/?guess=#{@guess}" if @guess
+end
+
+def assign_message
+	if @guess == "save"
+		"Game saved."
+	elsif @game.already_guessed.include? @guess
+		"Sorry, already guessed! Please try again."
+	else 
+		if @game.target.include? @guess 
+			"Correct!"
+		else
+			"Sorry, that guess is incorrect!"
+		end
+	end
+end
+
+# Find a way to implement this or something similar next time
+def play_again?
+	@answer2 = params["answer2"]
+	settings.game = Hangman.new if @answer2 == "yes"
+	"Thanks for playing!" if @answer2 == "no"
+end
 
 class Hangman
 	attr_accessor :target, :hidden_word, :already_guessed, :tries
 
-	# Word bank
 	words = File.open('5desk.txt', 'r')
 	@@hm_words = words.select { |line| line.length > 6 && line.length < 15 }
 
@@ -14,15 +77,14 @@ class Hangman
 		@hidden_word = []
 	end
 
-	# Load saved game or new game?
+
 	def self.load_game?
-		puts "Welcome to Hangman! Would you like to load your previous game?"
+		puts "Welcome to Hangman! Would you like to load your previous game? (Y/N)"
 		answer = gets.chomp.downcase
-		if answer == "y" or answer == "yes"
+		if answer[0] == "y"
 			puts "Loading game. You can save your game at any point by entering \"save\"."
-			self.from_json
-			self.play_again?
-		elsif answer == "n" or answer == "no"
+			self.from_json; self.play_again?
+		elsif answer[0] == "n"
 			self.new.run_game
 		else
 			puts "Sorry, unable to recognize your response."
@@ -30,72 +92,84 @@ class Hangman
 		end
 	end
 
-	# Conceal correct word
 	def hide_word
 		@target.length.times { @hidden_word.push("_") }
-		puts @hidden_word.join(" ")
 	end
 
-	# The game, in a nutshell
 	def run_game
 		puts "Starting new game. You can save your game at any point by entering \"save\"."
 		hide_word
-		enter_guess
+		puts; process_guess
 		Hangman.play_again?
 	end
 
-	# Option to save game
 	def save_game
-		puts "Saving game..."
+		#puts "Saving game..."
 		self.to_json
-		puts "Game saved."
+		"Game saved."
 	end
 
-	# Gets player input
-	def enter_guess
+	def process_guess(guess)
+		#display_tracker
+		#puts @hidden_word.join(" "); puts
 		unless @hidden_word.include? "_"
-			puts "You've got it! The word was #{@target}!"
+			"You've got it! The word was #{@target}!"
 		else
-			if @tries > 0
-				puts "Incorrect guesses left: #{tries}"
-				puts "Letters guessed: #{already_guessed}" unless @already_guessed.empty?
-				puts "Please choose a letter:"
-				guess = gets.chomp.downcase
-				pos = 0
-				check_against_target(guess, pos)
-				@already_guessed.push(guess) unless already_guessed.include? guess or guess == "save"
-				puts @hidden_word.join(" ")
-				enter_guess
-			else
-				puts "We're sorry, the correct word was #{target}!"
-			end
+			enter_guess(guess)
 		end
 	end
 
-	# Compares player input to correct word (or save option)
-	def check_against_target(guess, pos)
+	def display_tracker # Incorporated into erb
+		"Incorrect guesses remaining: #{@tries}"
+		"Letters guessed: #{@already_guessed}" unless @already_guessed.empty?
+	end
+
+	def choose_letter(guess)
+		pos = 0
+		#check_against_target(guess, pos)
 		if @already_guessed.include? guess
-			puts "Sorry, already guessed! Please try again."
-		elsif guess == "save"
+			"Sorry, already guessed! Please try again."
+		else
+			#@already_guessed.push(guess) unless guess == "save"
+			check_against_target(guess, pos)
+		end
+	end
+
+	def enter_guess(guess)
+		if @tries > 0
+			choose_letter(guess)
+			#process_guess(guess)
+		else
+			"Sorry, the correct word was #{@target}!"
+		end
+	end
+
+	# Add all this to the html? Idk
+	def check_against_target(guess, pos)
+		if guess == "save"
 			save_game
-		elsif @target == guess
-			while pos < @target.length
-				@hidden_word[pos].sub!("_", guess[pos]) if target[pos] == guess[pos]
-				pos += 1
-			end
-		elsif target.include? guess
-			puts "Correct!"
-			while pos < @target.length
-				@hidden_word[pos].sub!("_", guess) if target[pos] == guess
-				pos += 1
-			end
+		elsif @target.include? guess
+			update_hidden_word(guess, pos)
+			"Correct!"
 		else
 			@tries -= 1
-			puts "Sorry, that guess is incorrect!"
+			"Sorry, that guess is incorrect!"
+		end
+		"You've got it! The word was #{@target}!" unless @hidden_word.include? "_"
+	end
+
+	def update_hidden_word(guess, pos)
+		while pos < @target.length
+			if @target == guess
+				@hidden_word[pos].sub!("_", guess[pos]) if @target[pos] == guess[pos]
+			else
+				@hidden_word[pos].sub!("_", guess) if @target[pos] == guess
+			end
+			pos += 1
 		end
 	end
 
-	# Saving game to save file
+
 	def to_json
 		saved_game = File.open("saved_game.json", "w")
 		saved_game.puts JSON.dump ({
@@ -106,7 +180,6 @@ class Hangman
 		})
 	end
 
-	# Loading game from save file
 	def self.from_json
 		data = JSON.load (File.read("saved_game.json"))
 		save = self.new
@@ -114,17 +187,17 @@ class Hangman
 		save.tries = data['tries']
 		save.already_guessed = data['already_guessed']
 		save.hidden_word = data['hidden_word']
-		puts save.hidden_word.join (" ")
-		save.enter_guess
+		save
+		#puts save.hidden_word.join (" ")
+		#save.process_guess
 	end
 
-	# Pretty straightforward
 	def self.play_again?
-		puts "Would you like to play again?"
+		puts "Would you like to play again? (Y/N)"
 		answer = gets.chomp.downcase
-		if answer == "y" or answer == "yes"
+		if answer[0] == "y"
 			self.new.run_game
-		elsif answer == "n" or answer == "no"
+		elsif answer[0] == "n"
 			puts "Thanks for playing!"
 			exit
 		else
@@ -132,11 +205,11 @@ class Hangman
 			self.play_again?
 		end
 	end
-
 end
 
 
-Hangman.load_game?
+
+#Hangman.load_game?
 
 
 
@@ -144,9 +217,44 @@ Hangman.load_game?
 
 =begin
 
+		elsif @target == guess
+			while pos < @target.length
+				@hidden_word[pos].sub!("_", guess[pos]) if @target[pos] == guess[pos]
+				pos += 1
+			end
+		elsif @target.include? guess
+			puts "Correct!"
+			while pos < @target.length
+				@hidden_word[pos].sub!("_", guess) if @target[pos] == guess
+				pos += 1
+			end
+
 Other possible changes:
 -Refactoring/cleaning up code
 -Actual Hangman
 -Have game check if saved game already exists first
+
+_________
+|		|
+|	   ( )
+|      \|/
+|		|
+|		|
+|      / \
+|
+----------
+
+
+to start:
+_________
+|		
+|	   
+|      
+|		
+|		
+|      
+|
+----------
+
 
 =end
